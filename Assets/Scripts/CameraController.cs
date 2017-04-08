699ac94308d10cd5fcdd0c4f463674e9e3d71b19
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-04-08 15:45:44 -0300

    Fazendo a câmera seguir o jogador.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	//executa somente depois de tudo que estiver relacionado a Update() for executado
+	//assim, temos a certeza de que a câmera depois de tudo já ter se movido
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
